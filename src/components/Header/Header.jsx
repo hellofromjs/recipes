fd68262868f3ea997f0df18d2dcc5fe330cdc514
Moksleivis <donatas.tadaravicius@stud.kitm.lt>
2024-01-08 13:08:02 +0200

    added header padding

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -22,7 +22,7 @@ export default function Header() {
 
 	return (
 		<nav>
-			<header className="navbar navbar-expand-lg container d-flex justify-content-between header mt-3">
+			<header className="navbar navbar-expand-lg container d-flex justify-content-between header mt-3 px-2">
 				<Logo />
 				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					<span className="navbar-toggler-icon"></span>
